8981dd0a8040541dba02c5499b5b996222a80eeb
KrittikornS <122766587+KrittikornS@users.noreply.github.com>
2025-06-02 19:03:00 +0700

    update_verify_function

--- supports/services/booking/patchBooking.ts
+++ supports/services/booking/patchBooking.ts
@@ -14,11 +14,11 @@ class PatchBooking {
         expect(actualResult.statusCode, "statusCode should be 200").toEqual(200)
         expect(actualResult.statusText, "statusText should be OK").toEqual("OK")
         const expectData = Object.assign(defaultData, reqBody)
-        expect(actualResult.body.firstname, `firstname should be OK ${expectData.firstname}`).toEqual(expectData.firstname)
-        expect(actualResult.body.lastname, `lastname should be OK ${expectData.lastname}`).toEqual(expectData.lastname)
-        expect(actualResult.body.depositpaid, `depositpaid should be OK ${expectData.depositpaid}`).toEqual(expectData.depositpaid)
-        expect(actualResult.body.bookingdates, `bookingdates should be OK ${expectData.bookingdates}`).toEqual(expectData.bookingdates)
-        expect(actualResult.body.additionalneeds, `additionalneeds should be OK ${expectData.additionalneeds}`).toEqual(expectData.additionalneeds)
+        expect(actualResult.body.firstname, `firstname should be ${expectData.firstname}`).toEqual(expectData.firstname)
+        expect(actualResult.body.lastname, `lastname should be ${expectData.lastname}`).toEqual(expectData.lastname)
+        expect(actualResult.body.depositpaid, `depositpaid should be ${expectData.depositpaid}`).toEqual(expectData.depositpaid)
+        expect(actualResult.body.bookingdates, `bookingdates should be ${expectData.bookingdates}`).toEqual(expectData.bookingdates)
+        expect(actualResult.body.additionalneeds, `additionalneeds should be ${expectData.additionalneeds}`).toEqual(expectData.additionalneeds)
     }
 }
 
--- supports/services/booking/postBooking.ts
+++ supports/services/booking/postBooking.ts
@@ -9,11 +9,11 @@ class PostBooking {
         expect(actualResult.statusCode, "statusCode should be 200").toEqual(200)
         expect(actualResult.statusText, "statusText should be OK").toEqual("OK")
         expect(typeof actualResult.body.bookingid, "bookingid should be number").toEqual("number")
-        expect(actualResult.body.booking.firstname, `firstname should be OK ${reqBody.firstname}`).toEqual(reqBody.firstname)
-        expect(actualResult.body.booking.lastname, `lastname should be OK ${reqBody.lastname}`).toEqual(reqBody.lastname)
-        expect(actualResult.body.booking.depositpaid, `depositpaid should be OK ${reqBody.depositpaid}`).toEqual(reqBody.depositpaid)
-        expect(actualResult.body.booking.bookingdates, `bookingdates should be OK ${reqBody.bookingdates}`).toEqual(reqBody.bookingdates)
-        expect(actualResult.body.booking.additionalneeds, `additionalneeds should be OK ${reqBody.additionalneeds}`).toEqual(reqBody.additionalneeds)
+        expect(actualResult.body.booking.firstname, `firstname should be ${reqBody.firstname}`).toEqual(reqBody.firstname)
+        expect(actualResult.body.booking.lastname, `lastname should be ${reqBody.lastname}`).toEqual(reqBody.lastname)
+        expect(actualResult.body.booking.depositpaid, `depositpaid should be ${reqBody.depositpaid}`).toEqual(reqBody.depositpaid)
+        expect(actualResult.body.booking.bookingdates, `bookingdates should be ${reqBody.bookingdates}`).toEqual(reqBody.bookingdates)
+        expect(actualResult.body.booking.additionalneeds, `additionalneeds should be ${reqBody.additionalneeds}`).toEqual(reqBody.additionalneeds)
     }
 }
 
